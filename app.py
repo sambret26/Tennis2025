529639643c3caa6e249516acc9665628bc5b8c7d
sambret26 <samuelbretiere@orange.fr>
2024-12-16 23:37:22 +0100

    Allowed CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Configuration de la base de données (sera remplacée par une URL PostgreSQL sur Railway)
 import os
